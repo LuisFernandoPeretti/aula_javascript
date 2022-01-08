dfc9a2ada23baf48f278c7b9d119d2f6077aae63
Luis Fernando <nando.lfp@hotmail.com>
2022-01-08 17:14:23 -0300

    my first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Luís Fernando Peretti";
+var idade = 27;
+var idade2 = 10;
+var frase = "XV de JAÚ é o melhor time do mundo!!!";
+//alert(nome + " tem " + idade + " anos.");
+//alert(idade + idade2);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+console.log(frase.replace("XV de JAÚ", "São Paulo FC"));
+//alert(frase.replace("XV de JAÚ", "São Paulo FC"));
+console.log(frase.toUpperCase());
+console.log(frase.replace("XV de JAÚ", "São Paulo FC").toLowerCase());
