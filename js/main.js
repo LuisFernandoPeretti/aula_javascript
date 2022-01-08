f323d5273b98f1749945ef0acf47a76f0e000dd2
Luis Fernando <nando.lfp@hotmail.com>
2022-01-08 18:01:36 -0300

    teste 2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+var frutas = [{nome: "maçã", cor: "vermelho"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+/*
+var fruta = {nome: "maçã", cor: "vermelho"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+var lista = ["maçã", "peira", "laranja"];
+console.log(lista);
+console.log(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.reverse()[0]);
+console.log(lista.toString()[0]);
+console.log(lista.join(" | "));
+console.log(lista.join(""));
+*/
+
+/*
 var nome = "Luís Fernando Peretti";
 var idade = 27;
 var idade2 = 10;
@@ -11,3 +40,4 @@ console.log(frase.replace("XV de JAÚ", "São Paulo FC"));
 //alert(frase.replace("XV de JAÚ", "São Paulo FC"));
 console.log(frase.toUpperCase());
 console.log(frase.replace("XV de JAÚ", "São Paulo FC").toLowerCase());
+*/
